456e243717605b885238b5310e3966d55d17ff9f
Yuriy Chebyshev <ychebyshev@it.ru>
2022-03-21 23:06:22 +0300

    add select

--- app/handlers/dialogs/settings_reminder.py
+++ app/handlers/dialogs/settings_reminder.py
@@ -1,8 +1,8 @@
 from aiogram.dispatcher.fsm.state import StatesGroup, State
 from aiogram.types import CallbackQuery
 from aiogram_dialog import Dialog, Window, DialogManager
-from aiogram_dialog.widgets.kbd import Group, Button
-from aiogram_dialog.widgets.text import Const
+from aiogram_dialog.widgets.kbd import Button, ScrollingGroup, Select
+from aiogram_dialog.widgets.text import Const, Format
 
 
 class SettingsSG(StatesGroup):
@@ -11,20 +11,34 @@ class SettingsSG(StatesGroup):
     timetable = State()
 
 
-async def get_participants(c: CallbackQuery, button: Button, manager: DialogManager):
+async def get_participants(c: CallbackQuery, _: Button, manager: DialogManager):
     await c.answer("hello")
     await manager.dialog().switch_to(SettingsSG.participants)
+
+
+async def change_select():
+    pass
 
 
 dialog = Dialog(
     Window(
         Const("Настройка ежедневного митинга"),
-        Group(
-            Button(
-                Const("Подписчики"),
-                id="participants_ls",
-                on_click=get_participants,
+        Button(
+            Const("Подписчики"),
+            id="participants_ls",
+            on_click=get_participants,
+        ),
+        ScrollingGroup(
+            Select(
+                Format("{item}"),
+                id="participants",
+                item_id_getter=lambda x: x.username,
+                items="participants",
+                on_click=change_select,
             ),
+            id="participants_sg",
+            width=1,
+            height=10,
         ),
         state=SettingsSG.main,
     )
