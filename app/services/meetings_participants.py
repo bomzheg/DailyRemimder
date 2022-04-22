bbe7d6e289a8d9b3e1ec4b37a8d4953b680334f1
Yuriy Chebyshev <ychebyshev@it.ru>
2022-04-22 17:16:45 +0300

    fix forgotten commit

--- app/services/meetings_participants.py
+++ app/services/meetings_participants.py
@@ -12,3 +12,4 @@ async def turn_participant(
         holder_dao: HolderDao, meeting_id: int, asked_id: int
 ):
     await holder_dao.meeting.turn_participant(meeting_id, asked_id)
+    await holder_dao.commit()
